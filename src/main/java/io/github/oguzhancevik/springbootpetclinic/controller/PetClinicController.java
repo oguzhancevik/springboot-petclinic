d03248c8a9f0c5fd15fbbbaf63d1ee799f9da88e
Oğuzhan Çevik <cevik.ogsan@gmail.com>
2021-01-06 16:20:15 +0300

    Add Simple Controller Class

---
+++ src/main/java/io/github/oguzhancevik/springbootpetclinic/controller/PetClinicController.java
@@ -0,0 +1,16 @@
+package io.github.oguzhancevik.springbootpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PetClinicController {
+
+    @RequestMapping("/welcome")
+    @ResponseBody
+    public String welcome() {
+        return "Welcome";
+    }
+
+}
