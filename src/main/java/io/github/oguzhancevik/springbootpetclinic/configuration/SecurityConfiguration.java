6cba16c8f819b2a871ad08c6181814ced74341f1
Oğuzhan Çevik <cevik.ogsan@gmail.com>
2021-01-09 13:40:32 +0300

    Spring Security | Add Remember Me Button

--- src/main/java/io/github/oguzhancevik/springbootpetclinic/configuration/SecurityConfiguration.java
+++ src/main/java/io/github/oguzhancevik/springbootpetclinic/configuration/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package io.github.oguzhancevik.springbootpetclinic.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,5 +19,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.formLogin().loginPage("/login").failureUrl("/login?loginFailed=true");
+
+        http.rememberMe().userDetailsService(userDetailsService);
     }
 }
