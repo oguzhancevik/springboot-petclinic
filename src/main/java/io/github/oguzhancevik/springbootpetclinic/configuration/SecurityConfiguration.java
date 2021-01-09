ac9fc18328b4be6f26dbe99ca0257fa557034dac
Oğuzhan Çevik <cevik.ogsan@gmail.com>
2021-01-09 12:31:34 +0300

    Spring Security | Add Dependency and Create Security Config Class

---
+++ src/main/java/io/github/oguzhancevik/springbootpetclinic/configuration/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package io.github.oguzhancevik.springbootpetclinic.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**/favicon.ico", "/css/**", "js/**", "/images/**").permitAll()
+                .anyRequest().authenticated();
+        http.formLogin();
+    }
+}
